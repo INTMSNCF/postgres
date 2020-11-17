767181b640ab08aa1efd6a68f5a39c3ca3350dfe
Henry INTM <alograg@gmail.com>
2020-11-17 16:50:08 +0000

    fix(pgcrypto): INTM - SNCF version

--- contrib/pgcrypto/pgcrypto--1.0--1.1.sql
+++ contrib/pgcrypto/pgcrypto--1.0--1.1.sql
@@ -1,7 +1,7 @@
 /* contrib/pgcrypto/pgcrypto--1.0--1.1.sql */
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
-\echo Use "ALTER EXTENSION pgcrypto UPDATE TO '1.1'" to load this file. \quit
+\echo Use "ALTER EXTENSION pgcrypto UPDATE TO '1.1-intm-sncf'" to load this file. \quit
 
 CREATE FUNCTION gen_random_uuid()
 RETURNS uuid
